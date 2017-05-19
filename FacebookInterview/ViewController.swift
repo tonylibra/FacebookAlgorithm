269c8007231fa8202073c0a3da23c91e5ee596ed
tonylibra <tonylibra1018@gmail.com>
2017-05-18 21:25:26 -0400

    Initial Commit

---
+++ FacebookInterview/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FacebookInterview
+//
+//  Created by YangYusheng on 5/18/17.
+//  Copyright © 2017 yusheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
